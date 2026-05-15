17c737258b9391b3e18ac44106bce211dce64d0c
Immaculate Kimani <immakym001@gmail.com>
2026-05-15 16:08:44 +0300

    Integrated Firebase Authentication with Flutter

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Sign Up
   Future<User?> signUp(String email, String password) async {
     try {
       final result = await _auth.createUserWithEmailAndPassword(
